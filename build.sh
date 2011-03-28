805cbd5472ca88005eaaf9899c3a621c2ed79f71
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-03-28 22:58:29 +0400

    Fix paths.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ./bootstrap.sh
-PATH=$HOME/root/bin:$PATH ./bjam --prefix=$HOME/root/nacl64/ link=static --with-thread -d+2 --with-iostreams
-PATH=$HOME/root/bin:$PATH ./bjam install --prefix=$HOME/root/nacl64/ link=static --with-thread -d+2 --with-iostreams
+PATH=$HOME/root/nacl-sdk/bin:$PATH ./bjam --prefix=$HOME/root/nacl-sdk/nacl64/ link=static --with-thread -d+2 --with-iostreams
+PATH=$HOME/root/nacl-sdk/bin:$PATH ./bjam install --prefix=$HOME/root/nacl-sdk/nacl64/ link=static --with-thread -d+2 --with-iostreams
 
