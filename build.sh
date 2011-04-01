68795f54be26f370c1dadc7596bcdb1b3116d761
Evgeniy Stepanov <eugenis@google.com>
2011-04-01 15:09:54 +0400

    Add "usr" to prefix.

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
 ./bootstrap.sh
 LIBS="--with-thread --with-iostreams --with-regex --with-serialization --with-system"
-PATH=$ROOT/bin:$PATH ./bjam --prefix=$ROOT/nacl64/ link=static -d+2 ${LIBS}
-PATH=$ROOT/bin:$PATH ./bjam install --prefix=$ROOT/nacl64/ link=static ${LIBS}
+PATH=$ROOT/bin:$PATH ./bjam --prefix=$ROOT/nacl64/usr link=static -d+2 ${LIBS}
+PATH=$ROOT/bin:$PATH ./bjam install --prefix=$ROOT/nacl64/usr link=static ${LIBS}
 
