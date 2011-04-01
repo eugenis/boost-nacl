e4c10c1d2ebbe209269303f90072dd3e422fd587
Evgeniy Stepanov <eugenis@google.com>
2011-04-01 14:58:46 +0400

    Remove non-existant flag.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e -x
 ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
 ./bootstrap.sh
-LIBS="--with-thread --with-iostreams --with-regex --with-serialization --with-system --with-asio"
+LIBS="--with-thread --with-iostreams --with-regex --with-serialization --with-system"
 PATH=$ROOT/bin:$PATH ./bjam --prefix=$ROOT/nacl64/ link=static -d+2 ${LIBS}
 PATH=$ROOT/bin:$PATH ./bjam install --prefix=$ROOT/nacl64/ link=static ${LIBS}
 
