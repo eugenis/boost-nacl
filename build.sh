8ac95b44aefc3b261a475854f38d7023d9c03f6d
Evgeniy Stepanov <eugenis@google.com>
2011-09-10 22:41:18 +0400

    Update the arch prefix.

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
 ./bootstrap.sh
 LIBS="--with-thread --with-iostreams --with-regex --with-serialization --with-system"
-PATH=$ROOT/bin:$PATH ./bjam --prefix=$ROOT/nacl64/usr link=static -d+2 ${LIBS}
-PATH=$ROOT/bin:$PATH ./bjam install --prefix=$ROOT/nacl64/usr link=static ${LIBS}
+PATH=$ROOT/bin:$PATH ./bjam --prefix=$ROOT/x86_64-nacl/usr link=static -d+2 ${LIBS}
+PATH=$ROOT/bin:$PATH ./bjam install --prefix=$ROOT/x86_64-nacl/usr link=static ${LIBS}
 
